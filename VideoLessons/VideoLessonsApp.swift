67ac86448e3152f07f7e5cb1d37fff66636cd829
sujaloozou1 <sujaloozou@gmail.com>
2022-12-25 09:43:13 +0545

    Initial Commit

---
+++ VideoLessons/VideoLessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoLessonsApp.swift
+//  VideoLessons
+//
+//  Created by Sujal Shrestha on 25/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideoLessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
